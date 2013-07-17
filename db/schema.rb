ba7121787b5bfb7697b346a3aa2595c3f32b101b
David Fisher <tibbon+github@gmail.com>
2013-07-16 22:12:06 -0400

    Added password stuff for migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130715144420) do
+ActiveRecord::Schema.define(:version => 20130717020729) do
 
   create_table "favorites", :force => true do |t|
     t.integer  "user_id"
@@ -41,8 +41,9 @@ ActiveRecord::Schema.define(:version => 20130715144420) do
     t.string   "location"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
