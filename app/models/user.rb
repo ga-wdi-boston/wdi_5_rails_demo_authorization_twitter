16bda0d25182af2e1e86b75c605eab5756cac74c
David Fisher <tibbon+github@gmail.com>
2013-07-16 22:06:53 -0400

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
 	has_many :friends, through: :followings, source: 'followed'
 
 	attr_accessible :name, :screenname, :description, :email,
-					:location, :password
+					:location, :password, :password_confirmation
 
-	validates :screenname, presence: true
+	validates :screenname, presence: true, uniqueness: true, length: { minimum: 3 }
+	validates :password, confirmation: true, length: { minimum: 6 }
+	validates :password_confirmation, presence: true
 end
